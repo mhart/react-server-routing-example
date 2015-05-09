409cd911c22d6bbea1b008b439840615f0f8448a
Michael Hart <michael.hart.au@gmail.com>
2015-05-09 18:29:18 -0400

    Update react to 0.13.3, aws-sdk to 2.1.27 and browserify to 10.1.3

--- server.js
+++ server.js
@@ -60,8 +60,8 @@ var server = http.createServer(function(req, res) {
 
         // We'll load React and AWS from a CDN - you don't have to do this,
         // you can bundle them up or serve them locally if you like
-        script({src: '//fb.me/react-0.13.2.min.js'}),
-        script({src: '//sdk.amazonaws.com/js/aws-sdk-2.1.24.min.js'}),
+        script({src: '//fb.me/react-0.13.3.min.js'}),
+        script({src: '//sdk.amazonaws.com/js/aws-sdk-2.1.27.min.js'}),
 
         // Then the browser will fetch and run the browserified bundle consisting
         // of browser.js and all its dependencies.
