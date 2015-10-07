d767d8bd55537fbb7d6baa2e656bf4a84b0ad909
Michael Hart <michael.hart.au@gmail.com>
2015-10-07 14:02:54 -0400

    Update React to 0.14.0

--- server.js
+++ server.js
@@ -61,8 +61,8 @@ var server = http.createServer(function(req, res) {
 
         // We'll load React and AWS from a CDN - you don't have to do this,
         // you can bundle them up or serve them locally if you like
-        script({src: '//fb.me/react-0.14.0-rc1.min.js'}),
-        script({src: '//fb.me/react-dom-0.14.0-rc1.min.js'}),
+        script({src: '//fb.me/react-0.14.0.min.js'}),
+        script({src: '//fb.me/react-dom-0.14.0.min.js'}),
         script({src: '//sdk.amazonaws.com/js/aws-sdk-2.2.0.min.js'}),
 
         // Then the browser will fetch and run the browserified bundle consisting
